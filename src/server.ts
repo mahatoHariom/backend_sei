5ce144ce4b4b5606c7541736687bdc05b19e4bdb
mahatohariom <mahatohariom31@gmail.com>
2025-03-20 20:40:26 +0545

    update

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ const startServer = async () => {
       port: Number(process.env.PORT) || 9000,
       host: '0.0.0.0'
     })
+    
     console.log(`HTTP server running on http://localhost:${process.env.PORT}`)
   } catch (err) {
     console.error('Error starting server:', err)
